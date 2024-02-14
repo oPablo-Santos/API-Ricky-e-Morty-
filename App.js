d0f7782a0ec98b616bb796aa35f4672749cc290f
oPablo-Santos <pablo_p9_santos@hotmail.com>
2024-02-14 20:20:17 -0300

    API Ricky e morty v1
    Signed-off-by: oPablo-Santos <pablo_p9_santos@hotmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import api from "./src/services/Api";
 
 export default function App() {
+  const [characters, setCharacters] = useState([]);
+  const [episodes, setEpisodes] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await api.get("/character");
+        setCharacters(response.data.results);
+      } catch (error) {
+        console.error("Erro ao buscar personagens:", error);
+      }
+    };
+
+    fetchCharacters();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <FlatList
+        data={characters}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
